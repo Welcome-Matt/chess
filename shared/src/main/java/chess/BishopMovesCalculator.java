c8b1379214896ff43bbe909b4cef44e1c2812d4b
Matthew <mjt970@byu.edu>
2026-01-23 12:23:29 -0700

    Add overrides for equals and hashCode to both ChessPosition and ChessMove (they now pass the tests). Implemented BishopMovesCalculator (might be a better way to implement) and it passes the BishopMoveTests.

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -6,7 +6,66 @@ public class BishopMovesCalculator {
     
     public ArrayList<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> bishopMoves = new ArrayList<ChessMove>();
-        bishopMoves.add(new ChessMove(new ChessPosition(2,3), new ChessPosition(2,4), null));
+        int bishopRow = myPosition.getRow() + 1;
+        int bishopCol = myPosition.getColumn() + 1;
+        while(bishopRow < 9 && bishopCol < 9) {
+            if(board.getPiece(new ChessPosition(bishopRow, bishopCol)) == null) {
+                bishopMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(bishopRow, bishopCol), null));
+                bishopRow++;
+                bishopCol++;
+            } else if (board.getPiece(new ChessPosition(bishopRow, bishopCol)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                bishopMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(bishopRow, bishopCol), null));
+                break;
+            } else {
+                break;
+            }
+        }
+
+        bishopRow = myPosition.getRow() - 1;
+        bishopCol = myPosition.getColumn() + 1;
+        while(bishopRow > 0 && bishopCol < 9) {
+            if(board.getPiece(new ChessPosition(bishopRow, bishopCol)) == null) {
+                bishopMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(bishopRow, bishopCol), null));
+                bishopRow--;
+                bishopCol++;
+            } else if (board.getPiece(new ChessPosition(bishopRow, bishopCol)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                bishopMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(bishopRow, bishopCol), null));
+                break;
+            } else {
+                break;
+            }
+        }
+
+        bishopRow = myPosition.getRow() + 1;
+        bishopCol = myPosition.getColumn() - 1;
+        while(bishopRow < 9 && bishopCol > 0) {
+            if(board.getPiece(new ChessPosition(bishopRow, bishopCol)) == null) {
+                bishopMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(bishopRow, bishopCol), null));
+                bishopRow++;
+                bishopCol--;
+            } else if (board.getPiece(new ChessPosition(bishopRow, bishopCol)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                bishopMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(bishopRow, bishopCol), null));
+                break;
+            } else {
+                break;
+            }
+        }
+
+        bishopRow = myPosition.getRow() - 1;
+        bishopCol = myPosition.getColumn() - 1;
+        while(bishopRow > 0 && bishopCol > 0) {
+            if(board.getPiece(new ChessPosition(bishopRow, bishopCol)) == null) {
+                bishopMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(bishopRow, bishopCol), null));
+                bishopRow--;
+                bishopCol--;
+            } else if (board.getPiece(new ChessPosition(bishopRow, bishopCol)).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                bishopMoves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(bishopRow, bishopCol), null));
+                break;
+            } else {
+                break;
+            }
+        }
+
         return bishopMoves;
     }
 }
