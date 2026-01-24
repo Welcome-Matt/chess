a478751be01a2526e03b64ce652b0e69a790de04
Matthew <mjt970@byu.edu>
2026-01-23 19:44:55 -0700

    Implemented KnightMovesCalculator which passes the tests. Realized there might be a way to put the +/-2 and +/-1 into some kind of array and create a for loop.

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -5,8 +5,78 @@ import java.util.ArrayList;
 public class KnightMovesCalculator {
 
     public ArrayList<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        ArrayList<ChessMove> knightMoves = new ArrayList<ChessMove>();
-        knightMoves.add(new ChessMove(new ChessPosition(2,3), new ChessPosition(2,4), null));
+        ArrayList<ChessMove> knightMoves = new ArrayList<>();
+        int currentRow = myPosition.getRow();
+        int currentCol = myPosition.getColumn();
+        ChessGame.TeamColor pieceColor = board.getPiece(myPosition).getTeamColor();
+
+        int newRow = currentRow + 1;
+        int newCol = currentCol + 2;
+        if (newRow < 9 && newCol < 9) {
+            if (board.getPiece(new ChessPosition(newRow, newCol)) == null ||
+                    board.getPiece(new ChessPosition(newRow, newCol)).getTeamColor() != pieceColor) {
+                knightMoves.add(new ChessMove(new ChessPosition(currentRow, currentCol), new ChessPosition(newRow, newCol), null));
+            }
+        }
+
+        newRow = currentRow - 1;
+        if (newRow > 0 && newCol < 9) {
+            if (board.getPiece(new ChessPosition(newRow, newCol)) == null ||
+                    board.getPiece(new ChessPosition(newRow, newCol)).getTeamColor() != pieceColor) {
+                knightMoves.add(new ChessMove(new ChessPosition(currentRow, currentCol), new ChessPosition(newRow, newCol), null));
+            }
+        }
+
+        newRow = currentRow + 1;
+        newCol = currentCol - 2;
+        if (newRow < 9 && newCol > 0) {
+            if (board.getPiece(new ChessPosition(newRow, newCol)) == null ||
+                    board.getPiece(new ChessPosition(newRow, newCol)).getTeamColor() != pieceColor) {
+                knightMoves.add(new ChessMove(new ChessPosition(currentRow, currentCol), new ChessPosition(newRow, newCol), null));
+            }
+        }
+
+        newRow = currentRow - 1;
+        if (newRow > 0 && newCol > 0) {
+            if (board.getPiece(new ChessPosition(newRow, newCol)) == null ||
+                    board.getPiece(new ChessPosition(newRow, newCol)).getTeamColor() != pieceColor) {
+                knightMoves.add(new ChessMove(new ChessPosition(currentRow, currentCol), new ChessPosition(newRow, newCol), null));
+            }
+        }
+
+        newRow = currentRow + 2;
+        newCol = currentCol - 1;
+        if (newRow < 9 && newCol > 0) {
+            if (board.getPiece(new ChessPosition(newRow, newCol)) == null ||
+                    board.getPiece(new ChessPosition(newRow, newCol)).getTeamColor() != pieceColor) {
+                knightMoves.add(new ChessMove(new ChessPosition(currentRow, currentCol), new ChessPosition(newRow, newCol), null));
+            }
+        }
+
+        newCol = currentCol + 1;
+        if (newRow < 9 && newCol < 9) {
+            if (board.getPiece(new ChessPosition(newRow, newCol)) == null ||
+                    board.getPiece(new ChessPosition(newRow, newCol)).getTeamColor() != pieceColor) {
+                knightMoves.add(new ChessMove(new ChessPosition(currentRow, currentCol), new ChessPosition(newRow, newCol), null));
+            }
+        }
+
+        newRow = currentRow - 2;
+        if (newRow > 0 && newCol < 9) {
+            if (board.getPiece(new ChessPosition(newRow, newCol)) == null ||
+                    board.getPiece(new ChessPosition(newRow, newCol)).getTeamColor() != pieceColor) {
+                knightMoves.add(new ChessMove(new ChessPosition(currentRow, currentCol), new ChessPosition(newRow, newCol), null));
+            }
+        }
+
+        newCol = currentCol - 1;
+        if (newRow > 0 && newCol > 0) {
+            if (board.getPiece(new ChessPosition(newRow, newCol)) == null ||
+                    board.getPiece(new ChessPosition(newRow, newCol)).getTeamColor() != pieceColor) {
+                knightMoves.add(new ChessMove(new ChessPosition(currentRow, currentCol), new ChessPosition(newRow, newCol), null));
+            }
+        }
+
         return knightMoves;
     }
 }
