51085e20d6931916fbe9831b48deb6f3878ab38c
Matthew <mjt970@byu.edu>
2026-03-01 22:47:33 -0700

    Created some classes for user registration.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -11,7 +11,16 @@ public class UserService {
     public UserService(UserDAO userData) {
         this.userData = userData;
     }
-    //public RegisterResult register(RegisterRequest registerRequest) {}
+    public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
+        if (userData.getUser(registerRequest.username()) == null) {
+            UserData user = new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email());
+            userData.createUser(user);
+            return new RegisterResult(user.username(), user.password());
+        } else {
+            throw new DataAccessException("Error: username already taken");
+        }
+    }
+
     public LoginResult login(LoginRequest loginRequest) throws DataAccessException {
         UserData user = userData.getUser(loginRequest.username());
         return new LoginResult(user.username(), user.password());
