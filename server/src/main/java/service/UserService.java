e650de7378f02225c5c98a9e64dae326e78836a3
Matthew <mjt970@byu.edu>
2026-03-01 22:03:56 -0700

    Created some files and trying to get login to work.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,7 +1,20 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import model.UserData;
+
 public class UserService {
-    public RegisterResult register(RegisterRequest registerRequest) {}
-    public LoginResult login(LoginRequest loginRequest) {}
-    public void logout(LogoutRequest logoutRequest) {}
+
+    private final UserDAO userData;
+
+    public UserService(UserDAO userData) {
+        this.userData = userData;
+    }
+    //public RegisterResult register(RegisterRequest registerRequest) {}
+    public LoginResult login(LoginRequest loginRequest) throws DataAccessException {
+        UserData user = userData.getUser(loginRequest.username());
+        return new LoginResult(user.username(), user.password());
+    }
+    //public void logout(LogoutRequest logoutRequest) {}
 }
