481fc19d56e1735fd9c454ee21e844b0622b6a6e
Matthew <mjt970@byu.edu>
2026-03-13 20:13:22 -0600

    Implemented Sql class for user data.

---
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -0,0 +1,91 @@
+package dataaccess;
+
+import model.*;
+import java.sql.*;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
+public class MySqlUserDAO implements UserDAO {
+
+    public MySqlUserDAO() throws DataAccessException {
+        configureUserDAO();
+    }
+
+    public void createUser(UserData user) throws DataAccessException {
+        var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+        executeUpdate(statement, user.username(), user.password(), user.email());
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM user WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)){
+                ps.setString(1, username);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String SqlUsername = rs.getString("username");
+                        String SqlPassword = rs.getString("password");
+                        String SqlEmail = rs.getString("email");
+                        return new UserData(SqlUsername, SqlPassword,SqlEmail);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException("Error: unable to read data");
+        }
+
+        return null;
+    }
+
+    public void clearUserData() throws DataAccessException {
+        var statement = "TRUNCATE user";
+        executeUpdate(statement);
+    }
+
+    private String executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                }
+
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getString(1);
+                }
+
+                return "Hello";
+            }
+
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error: unable to update database");
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS user (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(password)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureUserDAO() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error: database failed");
+        }
+    }
+}
