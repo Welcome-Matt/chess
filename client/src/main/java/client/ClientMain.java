fe60574574551b4982143a3c6792fbf1e7d06862
Matthew <mjt970@byu.edu>
2026-03-24 20:28:37 -0600

    Started implementing some basic ui will probably move some of it to a different class.

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,29 @@
 package client;
 
 import chess.*;
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
 
 public class ClientMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            System.out.print("\n"+ SET_TEXT_COLOR_WHITE + ">>> ");
+            String line = scanner.nextLine();
+
+            try {
+                result = line;
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+
+        System.out.println();
     }
 }
