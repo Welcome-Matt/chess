f709f1fccf54134bdd9a5e45fa54055f4f8c014c
Matthew <mjt970@byu.edu>
2026-03-25 21:19:55 -0600

    Reformated some code to be more legible and preparing to add server facade.

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,6 +2,8 @@ package client;
 
 import chess.*;
 import ui.ChessUi;
+
+import java.util.Arrays;
 import java.util.Scanner;
 import static ui.EscapeSequences.*;
 
@@ -14,31 +16,25 @@ public class ClientMain {
 
         Scanner scanner = new Scanner(System.in);
         var result = "";
+
         while (!result.equals("quit")) {
             System.out.print("\n"+ SET_TEXT_COLOR_WHITE + "[" + status + "] " + ">>> ");
             String line = scanner.nextLine();
 
             try {
-                result = line;
+                String[] tokens = line.split(" ");
+                String cmd = (tokens.length > 0) ? tokens[0] : "help";
+                String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+                result = cmd;
+
                 if (status.equals("LOGGED_IN")) {
-                    postUi(result);
-                } else {
-                    switch (result) {
-                        case "white":
-                            ChessUi.main(new ChessBoard(), "White");
-                            break;
-                        case "black":
-                            ChessUi.main(new ChessBoard(), "Black");
-                            break;
-                        case "register":
-                        case "login":
-                            status = "LOGGED_IN";
-                            break;
-                        case "quit":
-                            break;
-                        default:
-                            preHelp();
+                    if (cmd.equals("quit")) {
+                        result = "Hamburger";
                     }
+
+                    postUi(cmd, params);
+                } else {
+                    preUi(cmd, params);
                 }
 
             } catch (Throwable e) {
@@ -46,12 +42,31 @@ public class ClientMain {
                 System.out.print(msg);
             }
         }
-
-        System.out.println();
     }
 
-    private static void postUi(String result) {
-        switch (result) {
+    private static void preUi(String cmd, String[] params) {
+        switch (cmd) {
+            case "white":
+                ChessUi.main(new ChessBoard(), "White");
+                break;
+            case "black":
+                ChessUi.main(new ChessBoard(), "Black");
+                break;
+            case "register":
+                System.out.print("You got registered\n");
+                break;
+            case "login":
+                status = "LOGGED_IN";
+                break;
+            case "quit":
+                break;
+            default:
+                preHelp();
+        }
+    }
+
+    private static void postUi(String cmd, String[] params) {
+        switch (cmd) {
             case "create":
                 ChessUi.main(new ChessBoard(), "White");
                 break;
@@ -72,53 +87,53 @@ public class ClientMain {
 
     private static void preHelp() {
         System.out.print(
-                        SET_TEXT_COLOR_BLUE +
-                        "  register <username> <password> <email>" +
-                        SET_TEXT_COLOR_MAGENTA +
-                        " - Create a chess account\n" +
-                        SET_TEXT_COLOR_BLUE +
-                        "  login <username> <password>" +
-                        SET_TEXT_COLOR_MAGENTA +
-                        " - Start playing chess\n" +
-                        SET_TEXT_COLOR_BLUE +
-                        "  quit" +
-                        SET_TEXT_COLOR_MAGENTA +
-                        " - Stop playing chess\n" +
-                        SET_TEXT_COLOR_BLUE +
-                        "  help" +
-                        SET_TEXT_COLOR_MAGENTA +
-                        " - Shows possible commands\n"
-                        + RESET_TEXT_COLOR
+            SET_TEXT_COLOR_BLUE +
+            "  register <username> <password> <email>" +
+            SET_TEXT_COLOR_MAGENTA +
+            " - Create a chess account\n" +
+            SET_TEXT_COLOR_BLUE +
+            "  login <username> <password>" +
+            SET_TEXT_COLOR_MAGENTA +
+            " - Start playing chess\n" +
+            SET_TEXT_COLOR_BLUE +
+            "  quit" +
+            SET_TEXT_COLOR_MAGENTA +
+            " - Stop playing chess\n" +
+            SET_TEXT_COLOR_BLUE +
+            "  help" +
+            SET_TEXT_COLOR_MAGENTA +
+            " - Shows possible commands\n"
+            + RESET_TEXT_COLOR
         );
     }
 
     private static void postHelp() {
         System.out.print(
-                        SET_TEXT_COLOR_BLUE +
-                        "  create <name>" +
-                        SET_TEXT_COLOR_MAGENTA +
-                        " - Creates a chess game\n" +
-                        SET_TEXT_COLOR_BLUE +
-                        "  list" +
-                        SET_TEXT_COLOR_MAGENTA +
-                        " - Shows a list of games\n" +
-                        SET_TEXT_COLOR_BLUE +
-                        "  join <id> <WHITE|BLACK>" +
-                        SET_TEXT_COLOR_MAGENTA +
-                        " - Join a game\n" +
-                        SET_TEXT_COLOR_BLUE +
-                        "  observe <id>" +
-                        SET_TEXT_COLOR_MAGENTA +
-                        " - Watch a game\n" +
-                        SET_TEXT_COLOR_BLUE +
-                        "  logout" +
-                        SET_TEXT_COLOR_MAGENTA +
-                        " - Logout of chess\n" +
-                        SET_TEXT_COLOR_BLUE +
-                        "  help" +
-                        SET_TEXT_COLOR_MAGENTA +
-                        " - Shows possible commands\n"
-                        + RESET_TEXT_COLOR
+            SET_TEXT_COLOR_BLUE +
+            "  create <name>" +
+            SET_TEXT_COLOR_MAGENTA +
+            " - Creates a chess game\n" +
+            SET_TEXT_COLOR_BLUE +
+            "  list" +
+            SET_TEXT_COLOR_MAGENTA +
+            " - Shows a list of games\n" +
+            SET_TEXT_COLOR_BLUE +
+            "  join <id> <WHITE|BLACK>" +
+            SET_TEXT_COLOR_MAGENTA +
+            " - Join a game\n" +
+            SET_TEXT_COLOR_BLUE +
+            "  observe <id>" +
+            SET_TEXT_COLOR_MAGENTA +
+            " - Watch a game\n" +
+            SET_TEXT_COLOR_BLUE +
+            "  logout" +
+            SET_TEXT_COLOR_MAGENTA +
+            " - Logout of chess\n" +
+            SET_TEXT_COLOR_BLUE +
+            "  help" +
+            SET_TEXT_COLOR_MAGENTA +
+            " - Shows possible commands\n"
+            + RESET_TEXT_COLOR
         );
     }
 }
